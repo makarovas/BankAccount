f53cc9d63cc468294d98ba33dca6089d728669ff
makarovas <makarofalex@gmail.com>
2024-03-16 20:12:34 +0300

    add createAccount, deposit functions

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >=0.4.22 <=0.8.17;
+pragma solidity ^0.8.17;
 
 contract BankAccount {
     constructor() {}
@@ -44,9 +44,57 @@ contract BankAccount {
     uint nextAccountId;
     uint nextWithdrawId;
 
-    function deposit(uint accountId) external payable {}
+    modifier accountOwner(uint accountId) {
+        bool isOwner;
+        for (uint idx; idx < accounts[accountId].owners.length; idx++) {
+            if (accounts[accountId].owners[idx] == msg.sender) {
+                isOwner = true;
+                break;
+            }
+        }
 
-    function createAccount(address[] calldata otherOwners) external {}
+        require(isOwner, "You are not the owner account");
+        _;
+    }
+
+    modifier validOwners(address[] calldata owners) {
+        require(owners.length + 1 <= 4, "maximum of 4 owners per account");
+        for (uint i; i < owners.length; i++) {
+            for (uint j = i + 1; j < owners.length; j++) {
+                if (owners[i] == owners[j]) {
+                    revert("no duplicate owners");
+                }
+            }
+        }
+        _;
+    }
+
+    function deposit(uint accountId) external payable accountOwner(accountId) {
+        accounts[accountId].balance += msg.value;
+    }
+
+    function createAccount(
+        address[] calldata otherOwners
+    ) external validOwners(otherOwners) {
+        // limit of the owners + contract owner
+        address[] memory owners = new address[](otherOwners.length + 1);
+        owners[otherOwners.length] = msg.sender;
+
+        uint id = nextAccountId;
+        for (uint idx; idx < owners.length; idx++) {
+            if (idx < owners.length - 1) {
+                owners[idx] = otherOwners[idx];
+            }
+            if (userAccounts[owners[idx]].length > 2) {
+                revert("each user can have a max of 3 accounts");
+            }
+            userAccounts[owners[idx]].push(id);
+        }
+
+        accounts[id].owners = owners;
+        nextAccountId++;
+        emit AccountCreated(owners, id, block.timestamp);
+    }
 
     function requestWithdrawl(uint accountId, uint amount) external {}
 
